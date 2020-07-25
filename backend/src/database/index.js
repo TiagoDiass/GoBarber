0dd552d09ed2e043ce7c20b7856c5d286285bfb2
TiagoDiass <tiago.costadiasss@gmail.com>
2020-07-25 17:24:20 -0300

    Making the relationship between users and their avatar

--- backend/src/database/index.js
+++ backend/src/database/index.js
@@ -2,8 +2,9 @@ import Sequelize from 'sequelize';
 import databaseConfig from '../config/database';
 
 import UserModel from '../app/models/User';
+import FileModel from '../app/models/File';
 
-const models = [UserModel];
+const models = [UserModel, FileModel];
 
 class Database {
   constructor() {
@@ -13,7 +14,11 @@ class Database {
   init() {
     this.connection = new Sequelize(databaseConfig);
 
-    models.map((model) => model.init(this.connection));
+    models
+      .map((model) => model.init(this.connection))
+      .map((model) => {
+        model.associate ? model.associate(this.connection.models) : () => {};
+      });
   }
 }
 
