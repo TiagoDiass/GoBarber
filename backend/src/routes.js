d2251400773bd4a2c0cb210758db1fbfade8a1be
TiagoDiass <tiago.costadiasss@gmail.com>
2020-07-24 21:01:56 -0300

    route to update an user completed

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,10 +1,16 @@
 import { Router } from 'express';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
+import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
 
-routes.post('/users/create', UserController.store);
 routes.post('/sessions', SessionController.store);
+routes.post('/users/create', UserController.store);
+
+// All the routes that come after this will use the authMiddleware
+routes.use(authMiddleware);
+
+routes.put('/users', UserController.update);
 
 export default routes;
