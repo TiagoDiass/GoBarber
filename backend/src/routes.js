19bcb4992732dd5e0bff30a9d7a56c48475b6c0f
TiagoDiass <tiago.costadiasss@gmail.com>
2020-07-29 19:19:57 -0300

    listing schedule done

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,4 +1,6 @@
-import { Router } from 'express';
+import {
+    Router
+} from 'express';
 import multer from 'multer';
 import multerConfig from './config/multer';
 
@@ -7,6 +9,7 @@ import SessionController from './app/controllers/SessionController';
 import ProviderController from './app/controllers/ProviderController';
 import FileController from './app/controllers/FileController';
 import AppointmentController from './app/controllers/AppointmentController';
+import ScheduleController from './app/controllers/ScheduleController';
 
 import authMiddleware from './app/middlewares/auth';
 
@@ -25,6 +28,8 @@ routes.get('/providers', ProviderController.index);
 
 routes.get('/appointments', AppointmentController.index);
 routes.post('/appointments', AppointmentController.store);
+
+routes.get('/schedule', ScheduleController.index)
 
 routes.post('/files', upload.single('file'), FileController.store);
 
