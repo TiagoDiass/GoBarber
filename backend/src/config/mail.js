3d3d6fdc88c1c3aa9fdc75c284f795aaecea1d2c
TiagoDiass <tiago.costadiasss@gmail.com>
2020-07-30 22:50:31 -0300

    e-mail feature added

---
+++ backend/src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  auth: {
+    user: 'b0e53d802be41b',
+    pass: 'fe0667aa72a791',
+  },
+  secure: false,
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
