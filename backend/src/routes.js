f589e2f2077db297b57c5e2a2fd6080304a2a349
TiagoDiass <tiago.costadiasss@gmail.com>
2020-07-31 23:45:28 -0300

    adding listing of hours

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,6 +1,4 @@
-import {
-    Router
-} from 'express';
+import { Router } from 'express';
 import multer from 'multer';
 import multerConfig from './config/multer';
 
@@ -11,6 +9,7 @@ import FileController from './app/controllers/FileController';
 import AppointmentController from './app/controllers/AppointmentController';
 import ScheduleController from './app/controllers/ScheduleController';
 import NotificationController from './app/controllers/NotificationController';
+import AvailableController from './app/controllers/AvailableController';
 
 import authMiddleware from './app/middlewares/auth';
 
@@ -26,6 +25,7 @@ routes.use(authMiddleware);
 routes.put('/users', UserController.update);
 
 routes.get('/providers', ProviderController.index);
+routes.get('/providers/:providerId/available', AvailableController.index);
 
 routes.get('/appointments', AppointmentController.index);
 routes.post('/appointments', AppointmentController.store);
